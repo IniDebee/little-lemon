754066b44d354b1ab64d64ffa640073ec0aba52e
Inioluwa <inioluwadeborah2@gmail.com>
2024-05-17 12:04:33 +0100

    Little lemon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import ProductDetails from "./routes/ProductDetails";
+import Header from "./components/Header";
+import BookingPage from "./routes/BookingPage";
+import ConfirmedBooking from "./routes/ConfirmedBooking";
 
 function App() {
   return (
-    <div className="App">
-      <header className="">Little Lemon</header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/product-detail" element={<ProductDetails />} />
+          <Route path="/reservation" element={<BookingPage />} />
+          <Route path="/thank-you" element={<ConfirmedBooking />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
